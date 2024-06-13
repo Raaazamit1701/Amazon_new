262536090a43cf64e7335bdf9e9b3e6ebc6fb933
amit raj <amitraaaz0@gmial.com>
2024-06-13 19:18:09 +0530

    addded

--- src/components/home/Products.jsx
+++ src/components/home/Products.jsx
@@ -36,71 +36,75 @@ const Products = () => {
   });
 
   return (
-    <div className='products-container max-w-screen-2xl mx-auto mt-10 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6 xl:gap-10 px-4'>
-      {filteredProducts.length > 0 ? (
-        filteredProducts.map((item) => (
-          <div key={item.id} className='bg-white h-auto border border-gray-200 py-8 z-30 hover:border-transparent shadow-lg hover:shadow-2xl transform hover:scale-105 transition duration-300 relative flex flex-col gap-4 rounded-lg'>
-            <span className='text-xs capitalize italic absolute top-2 right-5 text-gray-500'>{item.category}</span>
-            <div className='w-full h-auto flex items-center justify-center relative group'>
-              <img className='w-52 h-64 object-contain' src={item.image} alt={item.title} />
-              <ul className='w-full h-36 bg-gray-100 absolute bottom-[-170px] flex flex-col items-end justify-center gap-2 font-titlefont px-2 border-1 border-r group-hover:bottom-0 duration-700'>
-                <li className='productLi'>
-                  Compare <span><ApiIcon /></span>
-                </li>
-                <li className='productLi'>
-                  Add To Cart <span><ShoppingCartIcon /></span>
-                </li>
-                <li className='productLi' onClick={() => handleViewDetails(item)}>
-                  View Details <span><ArrowCircleRightIcon /></span>
-                </li>
-                <li className='productLi'>
-                  Add to WishList <span><FavoriteIcon /></span>
-                </li>
-              </ul>
-            </div>
-            <div className='px-4 z-10 bg-white'>
-              <div className='flex items-center justify-between'>
-                <h2 className='font-titleFont tracking-wide text-lg text-blue-700 font-bold'>
-                  {item.title.substring(0, 20)}
-                </h2>
-                <p className='text-sm text-gray-600 font-semibold'>${item.price}</p>
+    <div>
+      {/* Inline style for left-to-right animation */}
+      <style>
+        {`
+          .move-left-to-right {
+            transition: transform 0.8s ease-in-out;
+          }
+          .move-left-to-right:hover {
+            transform: translateX(20px);
+          }
+        `}
+      </style>
+      
+      <div className='products-container max-w-screen-2xl mx-auto mt-10 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6 xl:gap-10 px-4'>
+        {filteredProducts.length > 0 ? (
+          filteredProducts.map((item) => (
+            <div key={item.id} className='bg-white h-auto border border-gray-200 py-6 shadow-md relative flex flex-col gap-4 rounded-lg'>
+              <span className='text-xs capitalize italic absolute top-2 right-5 text-gray-500'>{item.category}</span>
+              <div className='w-full h-auto flex items-center justify-center relative'>
+                <img
+                  className='w-52 h-64 object-contain move-left-to-right'
+                  src={item.image}
+                  alt={item.title}
+                />
               </div>
-              <div>
-                <p className='text-sm text-gray-700'>{item.description.substring(0, 100)}...</p>
-                <div className='text-yellow-500 flex'>
-                  {[...Array(item.rating)].map((_, i) => (
-                    <StarIcon key={i} />
-                  ))}
+              <div className='px-4 z-10 bg-white'>
+                <div className='flex items-center justify-between'>
+                  <h2 className='font-titleFont tracking-wide text-lg text-blue-700 font-bold'>
+                    {item.title.length > 20 ? `${item.title.substring(0, 20)}...` : item.title}
+                  </h2>
+                  <p className='text-sm text-gray-600 font-semibold'>${item.price}</p>
                 </div>
+                <div>
+                  <p className='text-sm text-gray-700'>{item.description.length > 100 ? `${item.description.substring(0, 100)}...` : item.description}</p>
+                  <div className='text-yellow-500 flex'>
+                    {[...Array(item.rating)].map((_, i) => (
+                      <StarIcon key={i} />
+                    ))}
+                  </div>
+                </div>
+                <button onClick={() => dispatch(addToCart({
+                  id: item.id,
+                  title: item.title,
+                  description: item.description,
+                  price: item.price,
+                  category: item.category,
+                  image: item.image,
+                  quantity: 1 // assuming adding one item at a time
+                }))} className='w-full font-titlefont font-medium text-base bg-gradient-to-tr from-yellow-400 to-yellow-200 border border-yellow-500 hover:from-yellow-300 hover:to-yellow-300 hover:border-yellow-700 active:bg-gradient-to-bl active:from-yellow-400 active:to-yellow-500 duration-200 py-1.5 rounded-md mt-3'>Add to Cart</button>
               </div>
-              <button onClick={() => dispatch(addToCart({
-                id: item.id,
-                title: item.title,
-                description: item.description,
-                price: item.price,
-                category: item.category,
-                image: item.image,
-                quantity: 1 // assuming adding one item at a time
-              }))} className='w-full font-titlefont font-medium text-base bg-gradient-to-tr from-yellow-400 to-yellow-200 border border-yellow-500 hover:from-yellow-300 hover:to-yellow-300 hover:border-yellow-700 active:bg-gradient-to-bl active:from-yellow-400 active:to-yellow-500 duration-200 py-1.5 rounded-md mt-3'>Add to Cart</button>
+            </div>
+          ))
+        ) : (
+          <p className='col-span-full text-center text-xl text-gray-700'>No products available</p>
+        )}
+        {isModalOpen && selectedProduct && (
+          <div className='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50'>
+            <div className='bg-white p-4 rounded-lg max-w-lg w-full relative'>
+              <button onClick={closeModal} className='absolute top-2 right-2 text-gray-500 hover:text-gray-700'>
+                Close
+              </button>
+              <h2 className='text-xl font-bold mb-2'>{selectedProduct.title}</h2>
+              <img className='w-64 h-64 object-contain mx-auto' src={selectedProduct.image} alt={selectedProduct.title} />
+              <p className='text-gray-700 mt-4'>{selectedProduct.description}</p>
+              <p className='text-lg font-semibold mt-2'>${selectedProduct.price}</p>
             </div>
           </div>
-        ))
-      ) : (
-        <p className='col-span-full text-center text-xl text-gray-700'>No products available</p>
-      )}
-      {isModalOpen && selectedProduct && (
-        <div className='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50'>
-          <div className='bg-white p-4 rounded-lg max-w-lg w-full relative'>
-            <button onClick={closeModal} className='absolute top-2 right-2 text-gray-500 hover:text-gray-700'>
-              Close
-            </button>
-            <h2 className='text-xl font-bold mb-2'>{selectedProduct.title}</h2>
-            <img className='w-64 h-64 object-contain mx-auto' src={selectedProduct.image} alt={selectedProduct.title} />
-            <p className='text-gray-700 mt-4'>{selectedProduct.description}</p>
-            <p className='text-lg font-semibold mt-2'>${selectedProduct.price}</p>
-          </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
